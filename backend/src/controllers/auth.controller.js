228728d71f6acb4a8696b04f0eaee75b80a834cb
thegod71 <1011gauravsingh@gmail.com>
2026-09-04 18:24:32 +0530

    Signup setup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -26,7 +26,7 @@ export async function signup(req, res) {
       return res.status(400).json({ message: "Invalid email format" });
     }
 
-    const existingUser = User.findOne({ email });
+    const existingUser = await User.findOne({ email });
 
     if (existingUser) {
       return res
@@ -46,8 +46,18 @@ export async function signup(req, res) {
       expiresIn: "7d",
     });
 
-    await newUser.save();
-  } catch (err) {}
+    res.cookie("jwt", token, {
+      httpOnly: true, // it is use so that javascript can not access the cookie and it is only accessible by the  browser when   req come browser sent cookie to the  backend and here verify it then further process is done
+      secure: process.env.NODE_ENV === "production", // in .env we  development is write so secure=false means jwt is access by HTTP And HTTPS but when we deploy change to production then secure=true means jwt is access by HTTPS only not HTTP
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      sameSite: "strict", // it means only same site can access the cookie example that we login some website then on other pagesecond website open and this website try to access the cookie of first website then it is not allowed because sameSite is strict so only same site can access the cookie
+    });
+
+    res.status(201).json({ success: true, user: newUser });
+  } catch (err) {
+    console.error("error in auth controller", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 export function logout(req, res) {
   res.send("logout route");
