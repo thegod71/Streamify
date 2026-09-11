8b22f99fd4957618da922ce6a66e15a0e39e4827
thegod71 <1011gauravsingh@gmail.com>
2026-09-11 17:16:16 +0530

    All Controller

--- backend/src/controllers/user.controlles.js
+++ backend/src/controllers/user.controlles.js
@@ -1,3 +1,98 @@
-export async function getRecommendedUsers(params) {}
+import User from "../models/User.js";
+import FriendRequest from "../models/FriendRequest.js";
 
-export async function getMyFriends(params) {}
+export async function getRecommendedUsers(req, res) {
+  try {
+    const currentUserId = req.user.id;
+    const currentUser = req.user;
+    const recommendedUsers = User.find({
+      $and: [
+        { _id: { $ne: currentUserId } }, // exclude current user  $ne means:not equal
+        { $id: { $nin: currentUser.friends } }, // exclude friend  $nin means:not in
+        { isOnboarded: true }, //
+      ],
+    });
+    res.status(200).json(recommendedUsers);
+  } catch (err) {
+    console.log(err, "Error in the user Controllers");
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export async function getMyFriends(req, res) {
+  try {
+    const user = await User.findById(req.user.id)
+      .select("friends")
+      .populate(
+        "friends",
+        "fullName profilePic nativeLanguage learningLanguage",
+      );
+    //  findById = find user
+    //select = what information do I want?
+    //populate = ID ko actual user information mein badlo.
+    //     select = KYA chahiye?
+    // populate = ID ke peeche KAUN hai?
+
+    res.status(200).json(user.friends);
+  } catch {
+    console.log(err, "Error in the userControlles - GetmyFriend");
+    res.status(500).json({ message: "Internal server Error" });
+  }
+}
+
+export async function sendfriendRequest(req, res) {
+  try {
+    const myId = req.user.id;
+    const { id: recipientId } = req.params;
+
+    if (myId === recipientId) {
+      return res
+        .status(400)
+        .json({ message: "You can not sent Friend request to yourself" });
+    }
+
+    const recipient = await User.findById(recipientId);
+
+    if (!recipient) {
+      return res.status(400).json({ message: "Recipient not find" });
+    }
+
+    if (recipient.friends.includes(myId)) {
+      return res
+        .status(400)
+        .json({ message: "You are already Friend with this user" });
+    }
+
+    const existingRequest = await FriendRequest.findOne({
+      $or: [
+        { sender: myId, recipient: recipientId },
+        { sender: recipientId, recipient: myId },
+        //"Check whether a friend request already exists between these two people, no matter who sent it."
+      ],
+    });
+    if (existingRequest) {
+      return res
+        .status(400)
+        .json({ message: "You are already Friend Request with this user" });
+    }
+
+    const friendRequest = await FriendRequest.create({
+      sender: myId,
+      recipient: recipientId,
+    });
+
+    return res.status(200).json(friendRequest);
+  } catch (err) {
+    console.log(err, "Error in the user Controller sendfriends");
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export async function acceptfriendRequest(req, res) {
+  try {
+    const { id: requestId } = req.params;
+  } catch (error) {
+    console.log(err, "Error in the user Controller sendfriends");
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -3,6 +3,8 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getRecommendedUsers,
   getMyFriends,
+  sendfriendRequest,
+  acceptfriendRequest,
 } from "../controllers/user.controlles.js";
 
 const router = express.Router();
@@ -11,5 +13,7 @@ router.use(protectRoute); // Apply the protectRoute middleware to all routes in 
 
 router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
+router.post("/friend-request/:id", sendfriendRequest);
+router.put("/friend-request/:id/accept", acceptfriendRequest);
 
 export default router;
